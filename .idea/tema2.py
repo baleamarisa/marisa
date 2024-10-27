114353b719d2324657f6ed26dfbc935301a34b24
baleamarisa <Balea.Mt.Marisa@student.utcluj.ro>
2024-10-27 13:32:54 +0200

    nr 1

---
+++ .idea/tema2.py
@@ -0,0 +1,12 @@
+sir=(" Horoscop 21 octombrie 2024! ")
+nr_caractere_sir=len(sir)
+jumatate=(nr_caractere_sir//2)
+jumatate1=sir[:jumatate]
+jumatate2=sir[jumatate:]
+jumatate1=jumatate1.upper().strip()
+jumatate2=jumatate2.capitalize()
+jumatate2=jumatate2[::-1]
+jumatate2=jumatate2.replace('!','')
+print(jumatate1+jumatate2)
+
+
